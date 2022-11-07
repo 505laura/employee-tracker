5d05129615569b45385f395b1da6e449a98552a9
laura <laurabranc94@gmail.com>
2022-11-07 08:13:25 +0000

    Add mock view functions

--- index.js
+++ index.js
@@ -10,7 +10,43 @@ inquirer.prompt([{
     name: 'action',
     message: 'What would you like to do?',
     choices: options
-}])
-    .then((answers)=> console.log(answers));
+}]).then((answers) => {
 
+console.log(answers);
 
+switch (answers.action) {
+    case 'View all departments': {
+        console.table([
+            {name: 'Maths', id: 1},
+            {name: 'Computer Science', id: 2}
+        ]);
+        break;
+    }
+    case 'View all roles': {
+        console.table([
+            {name: 'Teacher', id: 1},
+            {name: 'Student', id: 2}
+        ]);
+        break;
+    }
+    case 'View all employees': {
+        console.table([
+            {name: 'Jasmine', id: 1},
+            {name: 'Peter', id: 2}
+        ]);
+        break;
+    }
+    case 'Add a department': {
+        break;
+    }
+    case 'Add a role': {
+        break;
+    }
+    case 'Add an employee': {
+        break;
+    }
+    case 'Update an employee role': {
+        break;
+    }
+}
+})
